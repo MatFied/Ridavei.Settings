e594dc113152e0a57f31c288ea3646cd181a855b
MatFied <mateuszfied@gmail.com>
2023-04-17 18:09:39 +0200

    Update IManager.cs

--- Ridavei.Settings/Interfaces/IManager.cs
+++ Ridavei.Settings/Interfaces/IManager.cs
@@ -2,19 +2,6 @@ namespace Ridavei.Settings.Interfaces
 {
     public interface IManager
     {
-        /// <summary>
-        /// Tries to retrieve the <see cref="ISettings"/> object for the specifed dictionary name.
-        /// </summary>
-        /// <param name="dictionaryName">Name of the dictionary</param>
-        /// <param name="settings">Retrieved Settings</param>
-        /// <returns>True if the Settings exists or false if not.</returns>
-        protected abstract bool TryGetSettingsObject(string dictionaryName, out ISettings settings);
-
-        /// <summary>
-        /// Creates the <see cref="ISettings"/> object for the specifed dictionary name.
-        /// </summary>
-        /// <param name="dictionaryName">Name of the dictionary</param>
-        /// <returns>Settings</returns>
-        protected abstract ISettings CreateSettingsObject(string dictionaryName);
+        
     } 
 }
