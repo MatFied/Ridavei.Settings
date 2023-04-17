74d58225fd4dc16ea4a072a1479ff6cf180ec22b
MatFied <mateuszfied@gmail.com>
2023-04-17 18:11:26 +0200

    Update AManager.cs

--- Ridavei.Settings/Base/AManager.cs
+++ Ridavei.Settings/Base/AManager.cs
@@ -73,6 +73,21 @@ namespace Ridavei.Settings.Base
                     _settings.Add(dictionaryName, res);
                 }
             return res;
+
+									/// <summary>
+        /// Tries to retrieve the <see cref="ISettings"/> object for the specifed dictionary name.
+        /// </summary>
+        /// <param name="dictionaryName">Name of the dictionary</param>
+        /// <param name="settings">Retrieved Settings</param>
+        /// <returns>True if the Settings exists or false if not.</returns>
+        protected abstract bool TryGetSettingsObject(string dictionaryName, out ISettings settings);
+
+        /// <summary>
+        /// Creates the <see cref="ISettings"/> object for the specifed dictionary name.
+        /// </summary>
+        /// <param name="dictionaryName">Name of the dictionary</param>
+        /// <returns>Settings</returns>
+        protected abstract ISettings CreateSettingsObject(string dictionaryName);
         }
     }
 }
